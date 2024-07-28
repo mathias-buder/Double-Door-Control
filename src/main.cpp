6678b085d5aa67c339687910e7c542790bb29317
Mathias Buder <mathias.buder@gmail.com>
2024-07-28 20:18:54 +0200

    Add RGB LED, push buttons, and relays to the circuit diagram

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,41 @@
 #include <Arduino.h>
 
+#define RBG_1_R         5
+#define RBG_1_G         6
+#define RBG_1_B         7
+
+#define TASTER_1        2
+#define TUERSCHALTER_1  3
+#define TUERMAGNET_1    4
+
 
 void setup() {
-  // put your setup code here, to run once:
-  digitalWrite(2, HIGH);
+  digitalWrite(RBG_1_R, HIGH);
+  digitalWrite(RBG_1_G, HIGH);
+  digitalWrite(RBG_1_B, HIGH);
+
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  
+  if( digitalRead(TUERSCHALTER_1) == HIGH )
+  {
+    digitalWrite(TUERMAGNET_1, HIGH);
+  }
+  else
+  {
+    digitalWrite(TUERMAGNET_1, LOW);
+  }
+
+  if(digitalRead(TASTER_1) == HIGH)
+  {
+    digitalWrite(RBG_1_G, HIGH);
+    digitalWrite(RBG_1_B, LOW);
+  }
+  else
+  {
+    digitalWrite(RBG_1_G, LOW);
+    digitalWrite(RBG_1_B, HIGH);
+  }
 }
