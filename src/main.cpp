a9dd77201c4aa9fd51503542baf525b6c56b59c0
Mathias Buder <mathias.buder@gmail.com>
2024-09-04 10:26:40 +0200

    Small updates

--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,44 @@
 
 #include "hsm.h"
 
-/****************************************************************************************/
-/*                                    PIN CONFIGURATION                                 */
-/****************************************************************************************/
+/***************************************************************************************************/
+/*                                           PIN CONFIGURATION                                     */
+/***************************************************************************************************/
 /* DOOR 1 */
-/****************************************************************************************/
-#define RBG_LED_1_R                     7       /*!< Pin for the red LED of the RGB-LED */
-#define RBG_LED_1_G                     6       /*!< Pin for the green LED of the RGB-LED */
-#define RBG_LED_1_B                     5       /*!< Pin for the blue LED of the RGB-LED */
-#define DOOR_1_BUTTON                   2       /*!< Pin for the button of the door */
-#define DOOR_1_SWITCH                   3       /*!< Pin for the switch of the door */
-#define DOOR_1_MAGNET                   4       /*!< Pin for the magnet of the door */
+/***************************************************************************************************/
+#define RBG_LED_1_R                     7              /*!< Pin for the red LED of the RGB-LED */
+#define RBG_LED_1_G                     6              /*!< Pin for the green LED of the RGB-LED */
+#define RBG_LED_1_B                     5              /*!< Pin for the blue LED of the RGB-LED */
+#define DOOR_1_BUTTON                   2              /*!< Pin for the button of the door */
+#define DOOR_1_SWITCH                   3              /*!< Pin for the switch of the door */
+#define DOOR_1_MAGNET                   4              /*!< Pin for the magnet of the door */
 
-/****************************************************************************************/
+/***************************************************************************************************/
 /* DOOR 2 */
-/****************************************************************************************/
-#define RBG_LED_2_R                     13      /*!< Pin for the red LED of the RGB-LED */
-#define RBG_LED_2_G                     12      /*!< Pin for the green LED of the RGB-LED */
-#define RBG_LED_2_B                     11      /*!< Pin for the blue LED of the RGB-LED */
-#define DOOR_2_BUTTON                   10      /*!< Pin for the button of the door */
-#define DOOR_2_SWITCH                   9       /*!< Pin for the switch of the door */
-#define DOOR_2_MAGNET                   8       /*!< Pin for the magnet of the door */
-/****************************************************************************************/
-/*                                GENERAL CONFIGURATION                                 */
-/****************************************************************************************/
-#define DEBOUNCE_DELAY_DOOR_BUTTON_1    100     /*!< Debounce delay for the door button @unit ms*/
-#define DEBOUNCE_DELAY_DOOR_BUTTON_2    100     /*!< Debounce delay for the door button @unit ms*/
-#define DEBOUNCE_DELAY_DOOR_SWITCH_1    100     /*!< Debounce delay for the door switch @unit ms*/
-#define DEBOUNCE_DELAY_DOOR_SWITCH_2    100     /*!< Debounce delay for the door switch @unit ms*/
-#define DEBOUNCE_STABLE_TIMEOUT         300     /*!< Timeout for the debounce stable state @unit ms */
+/***************************************************************************************************/
+#define RBG_LED_2_R                     13             /*!< Pin for the red LED of the RGB-LED */
+#define RBG_LED_2_G                     12             /*!< Pin for the green LED of the RGB-LED */
+#define RBG_LED_2_B                     11             /*!< Pin for the blue LED of the RGB-LED */
+#define DOOR_2_BUTTON                   10             /*!< Pin for the button of the door */
+#define DOOR_2_SWITCH                   9              /*!< Pin for the switch of the door */
+#define DOOR_2_MAGNET                   8              /*!< Pin for the magnet of the door */
+/***************************************************************************************************/
+/*                                        GENERAL CONFIGURATION                                    */
+/***************************************************************************************************/
+#define DEBOUNCE_DELAY_DOOR_BUTTON_1    100            /*!< Debounce delay for the door button @unit ms*/
+#define DEBOUNCE_DELAY_DOOR_BUTTON_2    100            /*!< Debounce delay for the door button @unit ms*/
+#define DEBOUNCE_DELAY_DOOR_SWITCH_1    100            /*!< Debounce delay for the door switch @unit ms*/
+#define DEBOUNCE_DELAY_DOOR_SWITCH_2    100            /*!< Debounce delay for the door switch @unit ms*/
+#define DEBOUNCE_STABLE_TIMEOUT         300            /*!< Timeout for the debounce stable state @unit ms */
 
-#define SERIAL_BAUD_RATE                115200  /*!< Baud rate of the serial communication @unit bps */
-#define LED_BLINK_INTERVAL              500     /*!< Interval of the led blink @unit ms */
-#define DOOR_UNLOCK_TIMEOUT             5       /*!< Timeout for the door unlock ( 0 = disabled ) @unit s */
-#define DOOR_OPEN_TIMEOUT               600     /*!< Timeout for the door open ( 0 = disabled ) @unit s */
+#define SERIAL_BAUD_RATE                115200         /*!< Baud rate of the serial communication @unit bps */
+#define DEFAULT_LOG_LEVEL               LOG_LEVEL_INFO /*!< Default log level */
 
-/************************************* ENUMERATION **************************************/
+#define LED_BLINK_INTERVAL              500            /*!< Interval of the led blink @unit ms */
+#define DOOR_UNLOCK_TIMEOUT             5              /*!< Timeout for the door unlock ( 0 = disabled ) @unit s */
+#define DOOR_OPEN_TIMEOUT               600            /*!< Timeout for the door open ( 0 = disabled ) @unit s */
+
+/********************************************** ENUMERATION ****************************************/
 
 /**
  * @brief Enumeration of the door control state
@@ -326,7 +328,11 @@ door_control_t doorControl = {
                 }
 };
 
-state_machine_t* const stateMachines[] = {(state_machine_t*) &doorControl}; /*!< Create and initialize the array of state machines. */
+/**
+ * @brief The array of state machines
+ * @details The array of state machines is used to dispatch the event to the state machines.
+ */
+state_machine_t* const stateMachines[] = {(state_machine_t*) &doorControl};
 
 /**
  * @brief The RGB LED pins
@@ -353,19 +359,19 @@ uint16_t sensorDebounceTime[SENSOR_SIZE] = {
  * @details The pin configuration is used to define the pin number and direction
  */
 pin_config_t pinConfig[] = {
-    /* Pin number,   Direction */
-    { RBG_LED_1_R,   OUTPUT },
-    { RBG_LED_1_G,   OUTPUT },
-    { RBG_LED_1_B,   OUTPUT },
-    { DOOR_1_BUTTON, INPUT  },
-    { DOOR_1_SWITCH, INPUT  },
-    { DOOR_1_MAGNET, OUTPUT },
-    { RBG_LED_2_R,   OUTPUT },
-    { RBG_LED_2_G,   OUTPUT },
-    { RBG_LED_2_B,   OUTPUT },
-    { DOOR_2_BUTTON, INPUT  },
-    { DOOR_2_SWITCH, INPUT  },
-    { DOOR_2_MAGNET, OUTPUT }
+    /* Pin number,      Direction */
+    { RBG_LED_1_R,      OUTPUT },
+    { RBG_LED_1_G,      OUTPUT },
+    { RBG_LED_1_B,      OUTPUT },
+    { DOOR_1_BUTTON,    INPUT  },
+    { DOOR_1_SWITCH,    INPUT  },
+    { DOOR_1_MAGNET,    OUTPUT },
+    { RBG_LED_2_R,      OUTPUT },
+    { RBG_LED_2_G,      OUTPUT },
+    { RBG_LED_2_B,      OUTPUT },
+    { DOOR_2_BUTTON,    INPUT  },
+    { DOOR_2_SWITCH,    INPUT  },
+    { DOOR_2_MAGNET,    OUTPUT }
 };
 
 /******************************** Function definition ************************************/
@@ -377,7 +383,7 @@ void setup()
 {
     /* Initialize with log level and log output */
     Serial.begin( SERIAL_BAUD_RATE );
-    Log.begin( LOG_LEVEL_INFO, &Serial );
+    Log.begin( DEFAULT_LOG_LEVEL, &Serial );
 
     Log.notice( "Starting ... " CR );
 
@@ -387,12 +393,10 @@ void setup()
         pinMode( pinConfig[i].number, pinConfig[i].direction );
     }
 
-    /* Initialize the doorControl */
-    // init( &doorControl );
-
     /* Initialize the led blink timer */
     Timer1.initialize( ( (uint32_t) 2000 ) * ( (uint32_t) LED_BLINK_INTERVAL ) );
 
+    /* Switch to the init state */
     switch_state( &doorControl.machine, &doorControlStates[DOOR_CONTROL_STATE_INIT] );
 
     Log.notice( "... Done" CR );
@@ -418,7 +422,13 @@ void loop()
 }
 
 
-
+/**
+ * @brief Handler for the init state entry
+ * 
+ * @param pState - The state machine
+ * @param event - The event
+ * @return state_machine_result_t - The result of the handler
+ */
 static state_machine_result_t initEntryHandler( state_machine_t* const pState, const uint32_t event )
 {
     Log.verbose("%s: Event %s" CR, __func__, eventToString( (door_control_event_t) event ).c_str() );
